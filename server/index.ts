ed2958ab2827660edafe1cd6c2d2f286d23ce5af
MikhailKayumov <mkaumov056@gamil.com>
2021-06-03 23:11:38 +0500

    init server

---
+++ server/index.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import mongo from 'mongoose';
+import config from 'config';
+
+// routes
+import AuthRouter from './routes/auth.routes';
+
+const PORT = config.get<number>('serverPort');
+const DB_URL = config.get<string>('dbUrl');
+
+const app = express();
+
+app.use(express.json());
+app.use('/api/auth', AuthRouter)
+
+const start = async () => {
+  try {
+    const db = await mongo.connect(DB_URL, {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+
+    await app.listen(PORT);
+  } catch (e) {
+    console.log(e);
+    process.exit(1);
+  }
+};
+
+start().then(() => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+});
